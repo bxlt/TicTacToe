3ad4a0c03c855de4e144d759f3fcdfcd7729c766
bxlt <xuelitan123@gmail.com>
2025-04-11 00:24:32 -0400

    add html and css

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const boardHtml = document.getElementById("board");
+
 const gameBoard = (function() {
     const board = [["","",""],["","",""],["","",""]];
-    const col = 3;
-    const row = 3;
+    const htmlRow = boardHtml.children;
+    const colNum = 3;
+    const rowNum = 3;
 
     const clear = () =>{
-        board = [["","",""],["","",""],["","",""]];
+        for(const row of board){
+            for(let i=0;i<colNum;i++){
+                row[i] = "";
+            }
+        }
+        
+        for (const row of htmlRow) {
+            for(const col of row.children){
+                col.textContent = "";
+                col.style.pointerEvents = 'auto';
+            }    
+        }
     }
 
     const printBoard = () => {
@@ -12,6 +26,8 @@ const gameBoard = (function() {
             console.log(row);
           });
     }
+
+    
 
     const checkWin = (sign) =>{
         //1.check row
@@ -21,21 +37,27 @@ const gameBoard = (function() {
     }
 
     const placeMark = (r,c,sign) =>{
-        if(board[r][c]===""){
+        console.log(r,c,sign);
+        let currNode = htmlRow[r].children[c];
+        //console.log(currNode);
+        if(!currNode.textContent){
+            currNode.textContent = sign;
+            currNode.style.pointerEvents = 'none';
             board[r][c] = sign;
         }
+
     }
 
     const checkRow = (sign)=>{
         let res = false;
-        for(let i=0;i<row;i++){
+        for(let i=0;i<rowNum;i++){
             let count = 0;
-            for(let j=0; j<col; j++){
+            for(let j=0; j<colNum; j++){
                 if(board[i][j]===sign){
                     count++;
                 }
             }
-            if(count==col){
+            if(count==colNum){
                 res = true;
                 break;
             }
@@ -45,14 +67,14 @@ const gameBoard = (function() {
 
     const checkCol = (sign) =>{
         let res = false;
-        for(let i=0;i<col;i++){
+        for(let i=0;i<colNum;i++){
             let count = 0;
-            for(let j=0; j<row; j++){
+            for(let j=0; j<rowNum; j++){
                 if(board[j][i]===sign){
                     count++;
                 }
             }
-            if(count==row){
+            if(count==rowNum){
                 res = true;
                 break;
             }
@@ -65,28 +87,36 @@ const gameBoard = (function() {
 
         let left2right = 0;
         let right2left = 0
-        for(let i=0;i<col;i++){
+        for(let i=0;i<colNum;i++){
             if(board[i][i]===sign){
                 left2right++;
             }
-            if(board[i][col-1-i]===sign){
+            if(board[i][colNum-1-i]===sign){
                 right2left++;
             }
         }
-        res = left2right==col || right2left==col;
+        res = left2right==colNum || right2left==colNum;
         return res;
+    }
+
+    const endGame = () =>{
+        for (const r of htmlRow){
+            for(const c of r.children){
+                c.style.pointerEvents = 'none';
+            }
+        }
     }
 
 
 
-    return {board,clear, placeMark, checkWin,printBoard};
+    return {board,colNum, rowNum, clear, placeMark, checkWin,endGame};
 })();
 
 
 //factory function for player obj
 function player(name,mark){
     
-    const point = 0;
+    let point = 0;
 
     const getPoint= ()=>{
         return point;
@@ -119,36 +149,97 @@ const Game = (function() {
     const board = gameBoard;
     const players = [player('1','X') ,player('2','O')];
     let turn = 0;
+    const htmlRow = boardHtml.childNodes;
+    let count =0;
 
     const changeTurn = () =>{
         turn = (turn+1)%players.length;
     }
 
+    const restart = ()=>{
+        turn = 0;
+        count=0;
+        board.clear();
+        players.forEach((p) =>p.reset());
+    }
+
+    const getMark = () =>{
+        return players[turn].mark;
+    }
+
+    const endGame = () =>{
+        board.endGame();
+    }
+
+
+    const setGame = () =>{
+        htmlRow.forEach((row) =>{
+            const cols = row.childNodes;
+            cols.forEach((col) =>{
+                
+                col.addEventListener('click',function(){
+                    if(this.textContent!=="") return;
+
+                    let id = parseInt(this.getAttribute('id').split("-")[1]);
+                    console.log(id)
+                    let currR = Math.floor(id/gameBoard.rowNum);
+                    let currC = id%gameBoard.colNum;
+                    board.placeMark(currR,currC, players[turn].mark);
+                    count++;
+                    console.log(count);
+                    if(count===9){
+                        console.log('Tie');
+                        endGame();
+                        return;
+                    }
+
+                    if(!board.checkWin(players[turn].mark)){
+                        changeTurn();
+                    }else {
+                        console.log("winner is " + turn);
+                        endGame();
+                        return;
+                    }
+                })
+            })
+        });
+    }
+
     const play = () => {
-        let winner;
         let isEnd = false;
         //show board before input
-        
+        setGame();
 
+       /*
         while(! isEnd){
-            gameBoard.printBoard();
+            //gameBoard.printBoard();
             let currP = players[turn];
-            let [currR, currC] = currP.askInput();
-            gameBoard.placeMark(currR, currC, currP.mark);
+            
+            //gameBoard.placeMark(currR, currC, currP.mark);
             //console()
             if(board.checkWin(currP.getMark())){
-                winner = turn;
+                
                 isEnd=true;
                 gameBoard.printBoard();
-            }else{
-                changeTurn();
+                console.log("winner is " + turn);
+            }else if(count==9){
+                isEnd = true;
+                console.log("Tie")
             }
-        }
-        console.log("winner is " + winner);
+        }*/
+        
     }
 
     
-    return {play};
+    return {play,restart};
 })();
 
+const restartBtn = document.getElementById("restart");
+restartBtn.addEventListener('click', function(e){
+    Game.restart();
+    Game.play();
+});
+
+
 Game.play();
+
